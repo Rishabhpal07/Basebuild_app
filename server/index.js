c68c937cd0e2812594b587d163a45612d804ced0
Rishabhpal07 <rishabhpal21390@gmail.com>
2025-09-06 23:17:57 +0530

    fixing minor bug in index.js file

--- server/index.js
+++ server/index.js
@@ -14,17 +14,17 @@ const path=require("path")
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   })
 );
-const_dirname = path. resolve();
+const __dirname = path.resolve();
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/user', require('./routes/user'));
 app.use('/api/message',require('./routes/message'))
 
-if(process. env.NODE_ENV==="production" ){
-  app.use (express.static(path.join(_dirname, "../client/dist")));
+if(process.env.NODE_ENV==="production" ){
+  app.use (express.static(path.join(__dirname, "../client/dist")));
 
  app.get ("*", (req, res) => {
-  res.sendFile(path.join(_dirname, "../client","dist", "index.html"))
+  res.sendFile(path.join(__dirname, "../client","dist", "index.html"))
 });
 }
 
